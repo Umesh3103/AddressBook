4cb4cdc586796255546bf5835f42bf55d9f4a24a
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-27 13:44:30 +0530

    [Umesh]Added: welcome message

---
+++ src/com/practice/AddressBookManagement.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+public class AddressBookManagement {
+	public static void main(String[] args) {
+		System.out.println("Welcome to address book management problem");
+	}
+
+}
